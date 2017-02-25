2a7ae889865f67960d9c3f6f82a75845606313ae
Tianbaba <htycm1@gmail.com>
2017-02-24 22:53:51 -0600

    Initial Commit

---
+++ Calculate_Pi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calculate_Pi
+//
+//  Created by Tianyi He on 2/24/17.
+//  Copyright © 2017 Tianyi He. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
